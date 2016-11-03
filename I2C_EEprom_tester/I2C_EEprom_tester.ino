585fd39953ed2ee052bc93f2e3013795008e1c02
EKMallon <EKMallon@users.noreply.github.com>
2016-11-02 21:36:16 -0500

    Comments

--- I2C_EEprom_tester/I2C_EEprom_tester.ino
+++ I2C_EEprom_tester/I2C_EEprom_tester.ino
@@ -3,30 +3,31 @@
  * Functions for R/W of single byte or a page of bytes. Max page is 28 bytes.
  */
 
-// from bHogan user post at http://forum.arduino.cc/index.php/topic,18501.0.html
+// this EEprom test utiilty is based on an original post by user bHogan user post at http://forum.arduino.cc/index.php/topic,18501.0.html
+// with various other useful bits of information added
 
 #include <Wire.h>
 #define EEPROM_ADDR 0x50          
-//  0x57  I2C Buss address of AT24C32 32K EEPROM with pins pulled high
-// EEprom address is 0x50 with pins pulled low
-#define startWriteAddressEEPROM 0  // you can just start this from 0
-#define pageSize 32                // for the AT24c32 - check datasheet for other chips
+// 0x57 I2C Bus address of AT24C32 4K EEPROM with pins pulled high
+// EEprom address for that chip series is 0x50 with pins pulled low (which matches the AT24C256 from edragon)
 
-int cyclesWritten2EEPROM;
-int currentPageStartAddress;
-int nextPageStartAddress;  //nextPageStartAddress = currentPageStartAddress +/- pageSize; 
+//some helpful variables if you start using page writes:
+#define pageSize 32                // this bytes per page limit imposed by the wire library
+int cyclesWritten2EEPROM;          // to keep track of when your eeprom is full
+int currentPageStartAddress=0;     // set to wherever you want the page writing to begin
+int nextPageStartAddress=0;        // set to wherever you want the page writing to begin
 
 void setup(){
   Wire.begin();                        // join I2C bus (address optional for master)
   
-  //Changing I2C speed  http://www.gammon.com.au/forum/?id=10896
-  //also http://electronics.stackexchange.com/questions/29457/how-to-make-arduino-do-high-speed-i2c
-  //if (EEPROM_ADDRESS==0x50){  
-  TWBR = 2;//for 400 kHz @ 8MHz CPU speed ONLY // AT24c256 ok @ 400kHz http://www.atmel.com/Images/doc0670.pdf  
+  // Changing I2C bus speed  http://www.gammon.com.au/forum/?id=10896
+  // also http://electronics.stackexchange.com/questions/29457/how-to-make-arduino-do-high-speed-i2c
+  // if (EEPROM_ADDRESS==0x50){  
+  // TWBR = 2;//for 400 kHz @ 8MHz CPU speed ONLY // AT24c256 ok @ 400kHz http://www.atmel.com/Images/doc0670.pdf  
   // see http://www.avrfreaks.net/forum/can-8mhz-clock-run-400khz-twi?skey=TWBR%208mhz 8mhz arduinos might not be capable of 400hz
-  //TWBR = 12; // 200kHz mode @ 8MHz or 400kHz mode @ 16MHz CPU 
-  //TWBR = 32;  // 100 kHz (default) @ 8MHz only 
-  //}
+  // TWBR = 12; // 200kHz mode @ 8MHz or 400kHz mode @ 16MHz CPU 
+  // TWBR = 32;  // 100 kHz (default) @ 8MHz only 
+  // }
   // AT24c256 ok to 400kHz http://www.atmel.com/Images/doc0670.pdf  
   // But the AT24c32 on the RTC board rated fro 100kHz  http://www.atmel.com/images/doc0336.pdf  
   // multispeed I2C scanner https://github.com/RobTillaart/Arduino/tree/master/sketches/MultiSpeedI2CScanner 
@@ -34,7 +35,6 @@ void setup(){
   // TWBR = ((F_CPU / TWI_FREQ) - 16) / 2;
   // SCL frequency = CPU_FREQUENCY / (16 + 2(TWBR) * (PrescalerValue)) //Prescalar=TWSR  // http://www.gammon.com.au/i2c    
   // for a CPU speed of 16000000 that will give:  TWBR =  ((16000000 / 100000) - 16) / 2 = 72 
-  
   
   Serial.begin(9600);
 
@@ -80,7 +80,11 @@ void loop(){
   //nothing here, only does the test once in the setup
 }
 
+// The functions listed below are not part of the eeprom tester, but are VERY handy to know about.
+// I've been using them for so long now, I no longer remember where I found them originally
 
+// EEprom PAGE read & write functions
+//===================================
 // Address is a page address, 6-bit (63). More and end will wrap around
 // But data can be maximum of 28 bytes, because the Wire library has a buffer of 32 bytes
 void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte* data, byte length ) {
@@ -93,6 +97,8 @@ void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte*
     Wire.write(data[c]);
   Wire.endTransmission();
   delay(10);                           // need some delay
+
+  //nextPageStartAddress = currentPageStartAddress +/- pageSize; 
 }
 
 // should not read more than 28 bytes at a time!
@@ -106,6 +112,9 @@ void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *bu
   for ( int c = 0; c < length; c++ )
     if (Wire.available()) buffer[c] = Wire.read();
 }
+
+// EEprom BYTE read & write functions
+//===================================
 
 byte i2c_eeprom_read_byte( int deviceaddress, unsigned int eeaddress ) {
   byte rdata = 0xFF;
