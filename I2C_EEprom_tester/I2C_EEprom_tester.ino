ca69a23c490b10356b05d49ad4a51b5ffd64d7cb
EKMallon <EKMallon@users.noreply.github.com>
2017-06-21 17:46:00 -0500

    updae
    update page write code

--- I2C_EEprom_tester/I2C_EEprom_tester.ino
+++ I2C_EEprom_tester/I2C_EEprom_tester.ino
@@ -86,33 +86,41 @@ void loop(){
 
 // EEprom PAGE read & write functions
 //===================================
-// Address is a page address, 6-bit (63). More and end will wrap around
-// But data can be maximum of 28 bytes, because the Wire library has a buffer of 32 bytes
-void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte* data, byte length ) {
-  //void eeprom_read_block (void * pointer_ram , const void * pointer_eeprom , size_t n)
+// see http://www.hobbytronics.co.uk/eeprom-page-write
+// Address is a page address - But data can be maximum of 28 bytes
+// The Arduino Wire library only has a 32 character buffer, so that is the maximun we can send using Arduino. 
+// This buffer includes the two address bytes which limits our data payload to 30 bytes, but I have never gotten this routine to 
+// work beyond 28 bytes of payload.
+// The time overhead for page writes  = 5ms, whilst the overhead for writing individual bytes = 3.5ms
+void Write_i2c_eeprom_page( int deviceaddress, unsigned int eeaddress, char* data) 
+{
+  unsigned char i=0;
+  unsigned int  address;
+  address=eeaddress;
   Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddresspage >> 8)); // Address High Byte
-  Wire.write((int)(eeaddresspage & 0xFF)); // Address Low Byte
-  byte c;
-  for ( c = 0; c < length; c++)  //this is the little loop that puts each character in 'data' char into the page
-    Wire.write(data[c]);
-  Wire.endTransmission();
-  delay(10);                           // need some delay
-
-  //nextPageStartAddress = currentPageStartAddress +/- pageSize; 
+  Wire.write((int)((address) >> 8));   // MSB
+  Wire.write((int)((address) & 0xFF)); // LSB
+  do{ 
+    Wire.write((byte) data[i]);
+    i++;
+  } 
+  while(data[i]);  
+  Wire.endTransmission(); 
+  //delay(6);  // data sheet says 5ms for page write
+  LowPower.powerDown(SLEEP_15MS, ADC_OFF, BOD_ON);
 }
 
-// should not read more than 28 bytes at a time!
-void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *buffer, int length ) {
+void Read_i2c_eeprom_page( int deviceaddress, unsigned int eeaddress,char* data, unsigned int num_chars) 
+{
+  unsigned char i=0;
   Wire.beginTransmission(deviceaddress);
-  Wire.write((int)(eeaddress >> 8));    // Address High Byte
-  Wire.write((int)(eeaddress & 0xFF));  // Address Low Byte
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
   Wire.endTransmission();
-  Wire.requestFrom(deviceaddress,length);
-  //int c = 0;
-  for ( int c = 0; c < length; c++ )
-    if (Wire.available()) buffer[c] = Wire.read();
+  Wire.requestFrom(deviceaddress,(num_chars-1));
+  while(Wire.available()) data[i++] = Wire.read();
 }
+
 
 // EEprom BYTE read & write functions
 //===================================
